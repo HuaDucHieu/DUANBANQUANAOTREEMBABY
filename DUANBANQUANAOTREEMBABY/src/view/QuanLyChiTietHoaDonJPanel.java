626520b5a1c62ab2d3b109a347ccb37d9b7d5bbc
HuaDucHieu <duchieuhua46@gmail.com>
2025-11-01 05:28:35 +0700

    khach hang xong

--- DUANBANQUANAOTREEMBABY/src/view/QuanLyChiTietHoaDonJPanel.java
+++ DUANBANQUANAOTREEMBABY/src/view/QuanLyChiTietHoaDonJPanel.java
@@ -446,14 +446,14 @@ public class QuanLyChiTietHoaDonJPanel extends javax.swing.JPanel {
                 .addComponent(txtTimKiem, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0)
                 .addComponent(btnTimKiem))
-            .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 705, Short.MAX_VALUE)
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 336, Short.MAX_VALUE)
                 .addComponent(btnThemSanPham)
                 .addGap(44, 44, 44)
                 .addComponent(btnCapNhat)
                 .addGap(44, 44, 44))
+            .addComponent(jScrollPane3)
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
